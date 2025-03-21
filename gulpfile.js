5f318e04d16eef424d3480b4f74b44c95e364360
Edwin Kofler <edwin@kofler.dev>
2025-03-21 03:50:18 -0700

    Upgrade dependencies and build configuration

--- gulpfile.js
+++ gulpfile.js
@@ -1,59 +1,35 @@
-const Webpack = require('webpack')
 const gulp = require('gulp')
-const webpack = require('gulp-webpack')
 const cssnano = require('gulp-cssnano')
 const rename = require('gulp-rename')
-const sass = require('gulp-sass')
-const del = require('del')
-const run = require('run-sequence')
+const sass = require('gulp-sass')(require('sass'))
+const { deleteAsync } = require('del')
+const terser = require('terser')
+const gulpTerser = require('gulp-terser')
 
-gulp.task('clean', () => del(['./dist']))
+exports.clean = function clean() {
+  return deleteAsync(['./dist'])
+}
 
-const webpackConfig = minimize => ({
-  output: {
-    filename: minimize ? 'notie.min.js' : 'notie.js',
-    library: 'notie',
-    libraryTarget: 'umd'
-  },
-  module: {
-    rules: [
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: ['babel-loader']
-      }
-    ]
-  },
-  plugins: minimize
-    ? [
-      new Webpack.optimize.UglifyJsPlugin({
-        compress: { warnings: false },
-        mangle: true,
-        sourcemap: false
-      })
-    ]
-    : []
-})
-
-gulp.task('script', () => {
-  gulp.src('./src/notie.js')
-  .pipe(webpack(webpackConfig(false), Webpack))
+exports.script = function script() {
+  return gulp.src('./src/notie.js')
   .pipe(gulp.dest('./dist'))
-  .pipe(webpack(webpackConfig(true), Webpack))
+  .pipe(gulpTerser({}, terser.minify))
+  .pipe(rename('notie.min.js'))
   .pipe(gulp.dest('./dist'))
-})
+}
 
-gulp.task('style', () => {
+exports.style = function style(cb) {
   gulp.src(['./src/notie.scss'])
   .pipe(sass().on('error', sass.logError))
   .pipe(gulp.dest('./dist'))
   .pipe(cssnano())
   .pipe(rename('notie.min.css'))
   .pipe(gulp.dest('./dist'))
-})
+  cb()
+}
 
-gulp.task('default', ['clean'], () => {
-  run('script', 'style')
-  gulp.watch('./src/notie.scss', ['style'])
-  gulp.watch('./src/notie.js', ['script'])
+exports.default = gulp.series(exports.clean, exports.script, exports.style, function (cb) {
+  gulp.watch('./src/notie.scss', exports.style)
+  gulp.watch('./src/notie.js', exports.script)
+  cb()
 })
